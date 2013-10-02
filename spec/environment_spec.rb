ec3ed028e903a6fb8e839c66539ddd20d0785518
Yehuda Katz <wycats@gmail.com>
2013-10-02 16:48:43 -0400

    Check in test

---
+++ spec/environment_spec.rb
@@ -0,0 +1,14 @@
+require "ninefold/environment"
+
+describe "Ninefold::Environment" do
+  let(:env) { { "NINEFOLD_TOKEN" => "some-token" } }
+  let(:wrapper) { Ninefold::Environment.new("NINEFOLD", env) }
+
+  it "looks up the name using the prefix" do
+    wrapper["TOKEN"].should == "some-token"
+  end
+
+  it "upcases the name" do
+    wrapper["token"].should == "some-token"
+  end
+end
