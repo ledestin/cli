a45a9e04a9b65d8ff2a20ac4530dea2d37840c96
Nikolay Nemshilov <nemshilov@gmail.com>
2014-03-17 12:09:11 +1100

    Adding basic Log#fetch method implementation

--- lib/ninefold/models/log.rb
+++ lib/ninefold/models/log.rb
@@ -1,3 +1,5 @@
+require "cgi"
+
 module Ninefold
   class Log
     attr_accessor :app, :options
@@ -10,6 +12,39 @@ module Ninefold
 
     def tailed?
       @options[:tail] == true
+    end
+
+    def fetch(&callback)
+      @host.get "/apps/#{@app.id}/logs?#{query_params}" do |response|
+        entries = response[:logs].map{|attrs| Entry.new(attrs) }
+        yield entries if block_given?
+      end
+    end
+
+    def query_params
+      params = {
+        from:   @options[:from],
+        to:     @options[:to],
+        host:   @options[:host],
+        source: @options[:source],
+        search: @options[:search]
+      }.reject! { |_,v| v == nil }
+
+      params.each{ |k,v| params[k] = v.utc.strftime("%FT%T.%LZ") if v.is_a?(Time) }
+      params.map { |k,v| "#{CGI.escape(k.to_s)}=#{CGI.escape(v.to_s)}" }.join("&")
+    end
+
+    class Entry
+      attr_reader :attributes
+
+      def initialize(attributes)
+        @attributes = attributes
+        @attributes['time'] = DateTime.parse(@attributes['time']).to_time if @attributes['time'].is_a?(String)
+      end
+
+      def method_missing(name, *args, &block)
+        @attributes[name.to_s] || super
+      end
     end
   end
 end
--- spec/ninefold/models/log_spec.rb
+++ spec/ninefold/models/log_spec.rb
@@ -1,8 +1,12 @@
 require "spec_helper"
 
 describe "Ninefold::Log" do
-  let(:app) { Ninefold::App.new }
+  let(:app) { Ninefold::App.new("id" => "123") }
   let(:options) { {tail: true} }
+  let(:host)  { Ninefold::Host.inst }
+  let(:entry_attrs) { {"id" => "asdf123", "time" => Time.now, "host" => "my-host-132", "source" => "rails", "message" => "Ohi there"} }
+  let(:entry) { Ninefold::Log::Entry.new(entry_attrs) }
+
   let(:log) { Ninefold::Log.new(app, options) }
 
   context "#initialize" do
@@ -24,6 +28,73 @@ describe "Ninefold::Log" do
     it "returns 'false' when options has :tail => false" do
       log.options = {tail: false}
       expect(log).not_to be_tailed
+    end
+  end
+
+  context "#fetch" do
+    let(:logs_url) { "/apps/#{app.id}/logs?search=love" }
+    let(:query) { log.fetch { |entries| @entries = entries } }
+
+    before { log.options = {search: "love"} }
+    before { host.respond_to(logs_url).with(:ok, {logs: [entry_attrs]}) }
+
+    it "makes query to the host" do
+      expect(host).to receive(:get).with(logs_url)
+      query
+    end
+
+    context "entries" do
+      let(:entries) { query && @entries }
+
+      it "is an array of one" do
+        expect(entries).to be_an Array
+      end
+
+      it "has an instance of a log entry" do
+        expect(entries[0]).to be_a Ninefold::Log::Entry
+      end
+
+      it "has the entry with correct properties" do
+        expect(entries[0].attributes.reject{|k,v| k == "time"}).to eq entry_attrs.reject{|k,v| k == "time"}
+      end
+    end
+  end
+
+  context "#query_params" do
+    it "merges options into a query string" do
+      log.options = {source: "rails", search: "love"}
+      expect(log.query_params).to eq "source=rails&search=love"
+    end
+
+    it "escapes the values" do
+      log.options = {search: "weird & stuff"}
+      expect(log.query_params).to eq "search=weird+%26+stuff"
+    end
+
+    it "exports dates into JSON compatible format" do
+      from = Time.now - 2000; to = Time.now + 2000
+      log.options = {from: from, to: to}
+      expect(log.query_params).to eq "from=#{CGI.escape(from.strftime("%FT%T.%LZ"))}&to=#{CGI.escape(to.strftime("%FT%T.%LZ"))}"
+    end
+  end
+
+  context "::Entry" do
+
+    it "has the assigned attributes" do
+      expect(entry.attributes).to eq entry_attrs
+    end
+
+    %w{id time host source message}.each do |property|
+      it "exposes #{property} as a method" do
+        expect(entry.__send__(property)).to eq entry_attrs[property]
+      end
+    end
+
+    it "parses time stamps from strings into dates" do
+      time  = Time.now
+      entry = Ninefold::Log::Entry.new("time" => time.utc.strftime("%FT%T.%LZ"))
+      expect(entry.time).to be_a Time
+      expect(entry.time.to_i / 10).to eq(time.to_i/10)
     end
   end
 end
